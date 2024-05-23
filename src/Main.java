62b0cfc0316c6d975e75d9affb18201494f26cfa
Baronas0927 <do.baublys@gmail.com>
2024-05-23 11:17:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> kelioniuGidas = new ArrayList<>();
+        
+
+    }
+}
+//        System.out.println("Hello world!");
+//        Scanner sc = new Scanner(System.in);
+//        ArrayList<String> shoppingList = new ArrayList();
+//        System.out.println();
+//        shoppingList.add("kirvis")
+//        while(true){
+//            System.out.println("ivesk preke");
+//            String input = sc.nextLine();
+//            shoppingList.add(input);
+//            System.out.println();
+//            for (int i = 0; i < shoppingList.size(); i++) {
+//                System.out.println(shoppingList.get(i));
+//            }
+//        }
